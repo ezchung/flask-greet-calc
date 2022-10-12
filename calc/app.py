f4e5bceea38452cd7577bda4f13b31c359a5c9a2
Christopher Lim <cli3338198@gmail.com>
2022-10-12 12:39:06 -0700

    second commit

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,29 @@
 # Put your app in here.
+from flask import Flask, request
+
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.get('/')
+def hello_world():
+  return "hello world"
+
+map = {
+  "add": add,
+  "sub": sub,
+  "mult": mult,
+  "div": div
+}
+
+
+@app.get("/<operation>")
+def operate(operation):
+
+  a = request.args["a"]
+  b = request.args["b"]
+
+  fn = map[operation]
+
+  return f"{fn(int(a), int(b))}"
+  
